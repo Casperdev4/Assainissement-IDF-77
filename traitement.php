8b9ff7868a14898cfc5da1a165cd7c8af5905136
Casperdev4 <allam.bilal91@gmail.com>
2025-05-07 00:47:32 +0200

    mlkjh

--- traitement.php
+++ traitement.php
@@ -74,7 +74,7 @@ try {
 
     $mail->send();
 
-    header('Location: https://assainissement94.com/');
+    header('Location: https://assainissement-77.fr/');
     exit();
 } catch (Exception $e) {
     echo "Message non envoyé. Erreur Mailer: {$mail->ErrorInfo}";
